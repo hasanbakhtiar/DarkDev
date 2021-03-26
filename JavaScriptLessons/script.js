9e1e9ed74e56529c2792350e6a00bcc48b6e0fb3
Hasan <hasan.doctype@gmail.com>
2021-03-26 20:10:05 +0400

    add JavaScript Lessons

---
+++ JavaScriptLessons/script.js
@@ -0,0 +1,29 @@
+// External Codes File
+
+// one line
+
+ /* more
+ line */
+
+//  document.write("JavaScript");
+//  document.writeln("Second JavaScript");
+
+// alert("hi js");
+// window.alert("hi js");
+
+// console.log("HI js");
+
+// document.write("1Hi JS <br>");
+// debugger;
+
+// document.write("2Hi JS <br>");
+// debugger;
+
+// document.write("3Hi JS <br>");
+// debugger;
+
+// document.write("4Hi JS <br>");
+// debugger;
+
+// document.write("5Hi JS <br>");
+// debugger;
