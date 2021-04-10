2c4dfc0163ad9a0fbf4077f0ce158f51d857859b
Hasan <hasan.doctype@gmail.com>
2021-04-10 13:15:00 +0400

    add JS Functions Lessons

---
+++ JavaScriptLessons/Lesson6/main.js
@@ -0,0 +1,59 @@
+// function name(params){
+//             doing...
+// }
+
+
+//ToDo App
+// function add1Item(x=5,y=10) {
+//     // let x = 5;
+//     // let y = 10;
+//     document.write(x+y);
+// }
+
+
+
+// add1Item(20,10);
+
+
+
+
+// function timeAll(timeAdd) {
+
+//     if (timeAdd == "morning") {
+//         document.write('time is: 05:00')
+//     }else if(timeAdd == "evening")
+//             document.write('time is: 17:00');
+//     else{
+//         document.write('please add time');
+//     }
+// }
+
+// const myTime = '';
+
+// timeAll(myTime);
+
+// function nameFunc(name) {
+//     document.write(name);
+// }
+
+
+// const myFunc = function(name) {
+//     document.write(name);
+    
+// }
+
+// myFunc("first");
+
+(function myFunc(item){
+
+    for (item ; item <= 10; item++) {
+        document.write(item + "<br>");
+        
+    }
+    var textLengt = "working my function";
+    console.log(textLengt.length);
+    
+})(5)
+
+
+
