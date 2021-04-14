13f122f6373c3e1cd8f16d889e1a6eea8631d433
Hasan <hasan.doctype@gmail.com>
2021-04-14 19:22:21 +0400

    add basic app

---
+++ excamples/main.js
@@ -0,0 +1,20 @@
+var car1Detail = () =>{
+    var car = {
+        model:"Mersedes",
+        price: 10000
+}
+
+
+document.getElementsByTagName('li')[0].innerHTML = car.model;
+document.getElementsByTagName('li')[1].innerHTML = car.price;
+}
+
+var carDetail = () =>{
+    var car1 = {
+        model:"BMW",
+        price: 20000
+}
+document.getElementsByTagName('li')[0].innerHTML = car1.model;
+document.getElementsByTagName('li')[1].innerHTML = car1.price;
+}
+
