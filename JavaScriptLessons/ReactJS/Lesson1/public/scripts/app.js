ea1833d6ae888759f072a50933529149f733de40
Hasan <hasan.doctype@gmail.com>
2021-05-19 20:11:17 +0400

    add ReactJS Lesson1

---
+++ JavaScriptLessons/ReactJS/Lesson1/public/scripts/app.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var root = document.getElementById('root');
+var infoData = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", {
+  className: ""
+}, "Hello World"), /*#__PURE__*/React.createElement("p", null, "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus eveniet id, est et tempora, saepe non autem tenetur voluptatum molestiae hic blanditiis delectus eum totam aut deserunt doloremque consequatur."));
+ReactDOM.render(infoData, root);
---
+++ JavaScriptLessons/ReactJS/Lesson1/src/app.js
@@ -0,0 +1,11 @@
+const root = document.getElementById('root');
+
+
+var infoData = <div>
+    <h1 className="">Hello World</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum necessitatibus eveniet id, est et tempora, saepe non autem tenetur voluptatum molestiae hic blanditiis delectus eum totam aut deserunt doloremque consequatur.</p>
+    
+</div>
+
+
+ReactDOM.render(infoData,root);
